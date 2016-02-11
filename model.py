f72a86ea8349abe1a5ec298d40958f3c2200bdfc
Uma Petersen <Uma@Umas-MacBook-Pro.local>
2016-02-11 11:10:18 -0800

    getting ready for deployment

---
+++ model.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+##############################################################################
+                ##### MODEL FOR USER TABLE ####
+##############################################################################
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    email = db.Column(db.String(50), nullable=True)
+    username = db.Column(db.String(20), nullable=True)
+    password = db.Column(db.String(128))
+   
+    
+##############################################################################
+                        ##### HELPER FUNCTIONS ####
+##############################################################################
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask App"""
+    DATABASE_URL = os.environ.get("DATABASE_URL",
+                              "postgresql:///livesimply")
+    DATABASE_URL = "postgresql:///livesimply"
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    # Prepare SQLAlchemy for connection
+    db.app = app
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Makes the connection
+    db.init_app(app)
+
+##############################################################################
+
+if __name__ == "__main__":
+# As a convenience, if we run this module interactively, it will leave
+# you in a state of being able to work with the database directly.
+
+    from routes import app
+    connect_to_db(app)
+    print "#### ** Conected to the Database ** ##"
---
+++ routes.py
@@ -0,0 +1,201 @@
+##############################################################################
+                ##### CONTROLLER, ROUTES, VIEW  ####
+##############################################################################
+import os
+from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for, flash, session
+from flask_wtf import Form
+from flask import jsonify
+from datetime import datetime
+from flask import send_from_directory
+from werkzeug import secure_filename
+from flask_debugtoolbar import DebugToolbarExtension
+# IMPORTED MODEL TABLES TO ROUTES
+from model import User
+from model import connect_to_db, db
+##############################################################################
+
+app = Flask(__name__)
+UPLOAD_FOLDER = '/uploads'
+app.secret_key = 'RED PANDA'
+##############################################################################
+
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "freedom")
+##############################################################################
+                        ## LOGIN ROUTE ##
+##############################################################################
+
+@app.route('/', methods=['GET'])
+def login():
+    """Show login form."""
+
+    return render_template("login.html")
+
+
+@app.route('/login-process', methods=['POST'])
+def process_login():
+    """Log user into site, find user in the DB and their
+    their user id in the session then if they
+     are logged in redirect them to map page"""
+
+    # Get form variables
+    email = request.form["email"]
+    password = request.form["password"]
+
+    # printing data from form to BASH
+    print "form password"
+
+    print password
+
+    # check user exisit and then asign them variable user
+    user = User.query.filter_by(email=email).first()
+
+    print "\n \n \n ", user
+
+    # Conditions
+    if not user:
+
+        flash("No such user")
+
+        return redirect("/")
+
+    elif user.password != password:
+
+        flash("Incorrect password")
+
+        return redirect("/")
+    else:
+        session["user_id"] = user.user_id
+
+    flash("Logged in")
+
+    return redirect('/index')
+
+##############################################################################
+                        ## LOG OUT ROUTE ##
+##############################################################################
+
+@app.route("/logout")
+def process_logout():
+    """removing user_id from session to logout user"""
+
+    print " LOGGED OUT USER "
+
+    del session["user_id"]
+
+    flash("You have Successfully Logged Out!")
+
+    return redirect("/")
+
+##############################################################################
+                            # # REGISTER ROUTE # #
+##############################################################################
+
+
+@app.route('/register-process', methods=['POST'])
+def register_processed():
+    """New user signup form"""
+
+    print "REGISTER ROUTE IS WORKING"
+
+    # Get variables from HTML form
+    email = request.form["email"]
+
+    password = request.form["password"]
+
+    # query the DB for user
+    new_user = User(email=email, password=password)
+
+    # check DB for user searching by email
+    same_email_user = User.query.filter(User.email == email).first()
+
+    # users who registered / login will be redircted --> list-page
+    if same_email_user:
+        flash("Email is already registered. Please signin to your account")
+        return redirect("/")
+
+    # check user by username --> condition to authentiate user
+    same_username = User.query.filter(User.email == email).first()
+    if same_username:
+        flash("please pick another username")
+        return redirect("/")
+
+        # add user to db if they are new
+    db.session.add(new_user)
+        # commit transaction
+    db.session.commit()
+
+    # query db user by email add them to current session and redirect
+    # user to passport page
+
+    user = User.query.filter_by(email=email).first()
+
+    flash("User %s added.You have successfully created an account! Welcome to Live Simply" % email)
+
+    session["user_id"] = user.user_id
+
+    return redirect("/index")
+
+##############################################################################
+                        # #  List Page # #
+##############################################################################
+
+@app.route('/index')
+def index():
+    """List page"""
+
+    user_id = session['user_id']
+
+
+
+    return render_template('index.html')
+
+
+##############################################################################
+@app.route('/contact')
+def contact():
+    """List page"""
+
+    user_id = session['user_id']
+
+
+
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    """List page"""
+
+    user_id = session['user_id']
+
+
+
+    return render_template('about.html')
+
+##############################################################################
+
+# @app.route('/index2')
+# def plainJS():
+#     """List page"""
+
+#     user_id = session['user_id']
+
+
+
+#     return render_template('index2.html')
+##############################################################################
+@app.route("/error")
+def error():
+    raise Exception("Error!")
+
+##############################################################################
+# HELPER FUNCTIONS
+##############################################################################
+
+
+if __name__ == "__main__":
+    
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+
+    app.run(debug=DEBUG, host="0.0.0.0", port=PORT)
